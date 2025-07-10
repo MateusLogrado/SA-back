67ffb132eed5f11018b93b3a552ea4ca492a1373
Mateus Logrado <mateus_logrado@estudante.sesisenai.org.br>
2025-07-10 09:02:33 -0300

    index completo

--- back/index.js
+++ back/index.js
@@ -9,6 +9,25 @@ const hostname = 'localhost'
 
 const conn = require('./db/conn')
 
+const compraController = require("./controller/compra.controller")
+const produtoController = require("./controller/produto.controller")
+const usuarioController = require("./controller/usuario.controller")
+
+app.post("/produto", produtoController.cadastrar)
+app.get("/produto", produtoController.listar)
+app.put("/produto", produtoController.atualizar)
+app.delete("/produto", produtoController.apagar)
+
+app.post("/usuario", usuarioController.cadastrar)
+app.get("/usuario", usuarioController.listar)
+app.put("/usuario", usuarioController.atualizar)
+app.delete("/usuario", usuarioController.apagar)
+
+app.post("/compra", compraController.cadastrar)
+app.get("/compra", compraController.listar)
+app.put("/compra", compraController.atualizar)
+app.delete("/compra", compraController.apagar)
+
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cors())
