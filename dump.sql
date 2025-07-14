e7714718bde85ff7cf31a9400b4978807df0698e
Mateus Logrado <mateus_logrado@estudante.sesisenai.org.br>
2025-07-14 09:45:23 -0300

    dump

---
+++ dump.sql
@@ -0,0 +1,124 @@
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: localhost    Database: compras_db
+-- ------------------------------------------------------
+-- Server version	8.0.40
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `compras`
+--
+
+DROP TABLE IF EXISTS `compras`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `compras` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `compraId` int NOT NULL,
+  `quant` int NOT NULL,
+  `dataCompra` datetime NOT NULL,
+  `unitario` float(10,2) NOT NULL,
+  `desconto` int NOT NULL,
+  `precoFinal` float(10,2) NOT NULL,
+  `formaPag` varchar(30) NOT NULL,
+  `status` varchar(30) NOT NULL,
+  `produto_id` int NOT NULL,
+  `usuario_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `produto_id` (`produto_id`),
+  KEY `usuario_id` (`usuario_id`),
+  CONSTRAINT `compras_ibfk_1` FOREIGN KEY (`produto_id`) REFERENCES `produtos` (`produtoId`),
+  CONSTRAINT `compras_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`usuarioId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `compras`
+--
+
+LOCK TABLES `compras` WRITE;
+/*!40000 ALTER TABLE `compras` DISABLE KEYS */;
+/*!40000 ALTER TABLE `compras` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `produtos`
+--
+
+DROP TABLE IF EXISTS `produtos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `produtos` (
+  `produtoId` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(35) NOT NULL,
+  `description` varchar(300) NOT NULL,
+  `category` varchar(35) NOT NULL,
+  `price` float(10,2) NOT NULL,
+  `discountPercentage` int NOT NULL,
+  `stock` int NOT NULL,
+  `brand` varchar(20) NOT NULL,
+  `thumbnail` varchar(100) NOT NULL,
+  PRIMARY KEY (`produtoId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `produtos`
+--
+
+LOCK TABLES `produtos` WRITE;
+/*!40000 ALTER TABLE `produtos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `produtos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `usuarioId` int NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(35) NOT NULL,
+  `lastName` varchar(35) NOT NULL,
+  `age` int NOT NULL,
+  `email` varchar(35) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `city` varchar(35) NOT NULL,
+  `state` varchar(35) NOT NULL,
+  `birthDate` date NOT NULL,
+  PRIMARY KEY (`usuarioId`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'luigi','italiano',18,'luigibros@gmail.pasta','454541','italia','cogumelo','1474-02-14'),(2,'luigi','italiano',18,'luigibros@gmail.pasta','454541','italia','cogumelo','1474-02-14'),(3,'luigi','italiano',18,'luigibros@gmail.pasta','454541','italia','cogumelo','1474-02-14'),(4,'luigi','italiano',18,'luigibros@gmail.pasta','454541','italia','cogumelo','1474-02-14'),(5,'luigi','italiano',18,'luigibros@gmail.pasta','454541','italia','cogumelo','1474-02-14'),(6,'luigi','italiano',18,'luigibros@gmail.pasta','454541','italia','cogumelo','1474-02-14'),(7,'luigi','italiano',18,'luigibros@gmail.pasta','454541','italia','cogumelo','1474-02-14'),(8,'luigi','italiano',18,'luigibros@gmail.pasta','454541','italia','cogumelo','1474-02-14'),(9,'luigi','italiano',18,'luigibros@gmail.pasta','454541','italia','cogumelo','1474-02-14'),(10,'luigi','italiano',18,'luigibros@gmail.pasta','454541','italia','cogumelo','1474-02-14'),(11,'luigi','italiano',18,'luigibros@gmail.pasta','454541','italia','cogumelo','1474-02-14'),(12,'luigi','italiano',18,'luigibros@gmail.pasta','454541','italia','cogumelo','1474-02-14'),(13,'luigi','italiano',18,'luigibros@gmail.pasta','454541','italia','cogumelo','1474-02-14'),(14,'luigi','italiano',18,'luigibros@gmail.pasta','454541','italia','cogumelo','1474-02-14'),(15,'luigi','italiano',18,'luigibros@gmail.pasta','454541','italia','cogumelo','1474-02-14');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-07-14  9:30:50
